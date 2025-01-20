f394fd59e8f747047d4d661aba5d6c0630567672
Ptichka Production <139170225+ptichkaproduction@users.noreply.github.com>
2025-01-20 23:32:43 +0300

    new list.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,8 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// friends variables
+const friendsItem = document.querySelectorAll("[data-friends-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -28,30 +28,30 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const friendsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < friendsItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
+  friendsItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalImg.src = this.querySelector("[data-friends-avatar]").src;
+    modalImg.alt = this.querySelector("[data-friends-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-friends-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-friends-text]").innerHTML;
 
-    testimonialsModalFunc();
+    friendsModalFunc();
 
   });
 
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", friendsModalFunc);
+overlay.addEventListener("click", friendsModalFunc);
 
 
 
